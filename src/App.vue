c6ee24b751fe321d488da5680c4c75840b6d5163
Ben Halverson <benhalverson33@gmail.com>
2020-05-09 22:15:14 -0700

    feat(todos app):
    todos list deletion works

---
+++ src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div id="app">
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+  </div>
+</template>
+
+<script>
+import Todos from "./components/Todos";
+export default {
+  name: "App",
+  components: {
+    Todos
+  },
+  methods: {
+    deleteTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id != id);
+    }
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "todo one",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "todo two",
+          completed: true
+        },
+        {
+          id: 3,
+          title: "todo two",
+          completed: true
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+</style>
