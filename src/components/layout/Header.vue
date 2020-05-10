19797b2f62c43d192d5972e0758a02959ca980d4
Ben Halverson <benhalverson33@gmail.com>
2020-05-09 23:11:43 -0700

    feat(router): Added vue router

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,11 +1,15 @@
 <template>
   <header class="header">
     <h1>todo list</h1>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
   </header>
 </template>
 <script>
 export default {
-  name: "Header"
+  name: 'Header'
 };
 </script>
 
@@ -19,5 +23,6 @@ export default {
 .header a {
   color: #ffffff;
   padding-right: 5px;
+  text-decoration: none;
 }
 </style>
